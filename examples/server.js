a9dcf8fa07f601a73ad8fd353128a88c7a89e28d
thomascxq <thomascxq@gmail.com>
2016-07-23 21:20:35 +0800

    Init a demo server.

---
+++ examples/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.set('host', process.env.HOST || '0.0.0.0');
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(app.get('port'), app.get('host'), (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://' + app.get('host') + ':' + app.get('port'));
+});
