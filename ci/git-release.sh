f348c87d9114fbb5121d7538b1226e292475d047
thomascxq <thomascxq@gmail.com>
2016-07-31 23:00:27 +0800

    Add git release.

---
+++ ci/git-release.sh
@@ -0,0 +1,20 @@
+#! bin/bash
+
+CURRENT_VERSION=`cat package.json | jq -r .version`
+
+git fetch --tags
+
+if [ `git branch | grep tmp-release` ]; then
+    git branch -D tmp-release
+fi
+git checkout -b tmp-release
+
+npm install
+npm run build
+git add -f dist/ lib/
+git commit -m "Release v$CURRENT_VERSION"
+git tag "v$CURRENT_VERSION"
+git push --tags
+
+git checkout master
+git branch -D tmp-release
