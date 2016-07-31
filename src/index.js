32f06d8fa96e3722767072b32b902eeae848789c
thomascxq <thomascxq@gmail.com>
2016-07-31 19:14:29 +0800

    Update export file and scripts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export HelloWorld from './js/HelloWorld';
 export BaseController from './js/BaseController';
 export ControllerLoader from './js/ControllerLoader';
 export ControllerActionLoader from './js/ControllerActionLoader';
